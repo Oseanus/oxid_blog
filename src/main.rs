5edd68ebb3b3af111a2ca6277567e006eddef98a
Oliver Ellis <ellis.oliver@gmail.com>
2023-08-25 23:20:59 +0200

    Fixed missing letter in extension

--- src/main.rs
+++ src/main.rs
@@ -17,5 +17,5 @@ async fn main() {
 async fn handler() -> Html<&'static str> {
     // `std::include_str` macro can be used to include an utf-8 file as `&'static str` in compile
     // time. This method is relative to current `main.rs` file.
-    Html(include_str!("../static/index.htm"))
+    Html(include_str!("../static/index.html"))
 }
