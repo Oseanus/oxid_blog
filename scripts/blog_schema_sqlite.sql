f44647004c1737a588586f322c43d00045cd1e93
Oliver Ellis <ellis.oliver@gmail.com>
2023-08-25 23:59:00 +0200

    Updated script

--- scripts/blog_schema_sqlite.sql
+++ scripts/blog_schema_sqlite.sql
@@ -12,8 +12,8 @@ CREATE TABLE users (
     reset_password_selector VARCHAR,
     reset_password_sent_at TIMESTAMP,
     reset_password_validator_hash VARCHAR,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW,
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO users(email, hashed_password) VALUES('test@test.com', 'abcd');
@@ -22,12 +22,20 @@ CREATE TABLE sessions (
     id SERIAL PRIMARY KEY, 
     session_verifier VARCHAR NOT NULL, 
     user_id INT NOT NULL, 
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     otp_code_encrypted VARCHAR NOT NULL,
     otp_code_attempts INTEGER NOT NULL DEFAULT 0,
     otp_code_confirmed BOOLEAN NOT NULL DEFAULT false,
     otp_code_sent BOOLEAN NOT NULL DEFAULT false
 );
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    created_at TIMSTAMPZ DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO posts (title) VALUES ('SQLite NOW function');
 
 -- DROP TABLE users;
 -- DROP TABLE sessions;
