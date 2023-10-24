f83e3d13fc21e7e036561d36a04f83b1163e6931
Oliver Ellis <ellis.oliver@gmail.com>
2023-10-24 12:36:10 +0200

    Refactored database operations in seperate file

---
+++ src/database.rs
@@ -0,0 +1,24 @@
+extern crate mongodb;
+
+use mongodb::{ Client, options::ClientOptions, bson::{doc, Document} };
+use crate::CustomError;
+
+pub async fn connect_database(auth_string: String) -> Client {
+    let client_options = ClientOptions::parse(auth_string).await.unwrap();
+    let client = Client::with_options(client_options).unwrap();
+    client
+}
+
+pub async fn insert_document(client: &Client, database_name: &str, collection_name: &str, document: Document) {
+    let database = client.database(database_name);
+    let collection = database.collection(collection_name);
+    collection.insert_one(document, None).await.unwrap();
+}
+
+pub async fn get_document(client: &Client, database_name: &str, collection_name: &str, filter: Document) -> Result<Document, CustomError> {
+    let database = client.database(database_name);
+    let collection = database.collection::<Document>(collection_name);
+    let result = collection.find_one(filter, None).await.unwrap();
+
+    result.ok_or_else(|| CustomError::Database("Error: Failed to get document from database.".to_string()))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 extern crate mongodb;
 mod config;
 mod errors;
+mod database;
 
 use axum::{ routing::get, Router, response::{Html, IntoResponse}, http::StatusCode, extract::State };
-use mongodb::{ Client, options::ClientOptions, bson::{doc, Document} };
+use mongodb::{ Client, bson::doc };
 use askama::Template;
 use errors::CustomError;
 use std::net::SocketAddr;
@@ -24,7 +25,7 @@ async fn index(State(state): State<Arc<Client>>) -> impl IntoResponse {
     let collection_name = "users";
     let filter = doc! {"name": "Oliver"};
 
-    let user_doc = get_document(&client, database_name, collection_name, filter).await;
+    let user_doc = database::get_document(&client, database_name, collection_name, filter).await;
     let user = user_doc.unwrap();
 
     let template = User {
@@ -46,7 +47,7 @@ async fn index(State(state): State<Arc<Client>>) -> impl IntoResponse {
 async fn main() {
     let config = config::Config::new(String::from("127.0.0.1"), 27017, String::from("oliver"), String::from("Stoneenge"));
     let auth_string = config.get_auth_string();
-    let client = connect_database(auth_string).await;
+    let client = database::connect_database(auth_string).await;
 
     let shared_state = Arc::new(client);
 
@@ -61,24 +62,4 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
-}
-
-async fn connect_database(auth_string: String) -> Client {
-    let client_options = ClientOptions::parse(auth_string).await.unwrap();
-    let client = Client::with_options(client_options).unwrap();
-    client
-}
-
-async fn insert_document(client: &Client, database_name: &str, collection_name: &str, document: Document) {
-    let database = client.database(database_name);
-    let collection = database.collection(collection_name);
-    collection.insert_one(document, None).await.unwrap();
-}
-
-async fn get_document(client: &Client, database_name: &str, collection_name: &str, filter: Document) -> Result<Document, CustomError> {
-    let database = client.database(database_name);
-    let collection = database.collection::<Document>(collection_name);
-    let result = collection.find_one(filter, None).await.unwrap();
-
-    result.ok_or_else(|| CustomError::Database("Error: Failed to get document from database.".to_string()))
 }
